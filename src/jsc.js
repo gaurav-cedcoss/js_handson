cddb70b5ed094de664f7920039d47a3fac99d21c
gaurav-cedcoss <gauravshukla2022jan@cedcoss.com>
2022-02-05 17:25:28 +0530

    Testing Objects for Properties

--- src/jsc.js
+++ src/jsc.js
@@ -1,19 +1,11 @@
-// Setup
-function phoneticLookup(val) {
-  let result = "";
-
+function checkObj(obj, checkProp) {
   // Only change code below this line
-  var lookup= {
-    "alpha":"Adams",
-    "bravo":"Boston",
-   "charlie":"Chicago",
-   "delta":"Denver",
-  "echo":"Easy",
-    "foxtrot":"Frank"};
+  if(obj.hasOwnProperty(checkProp))
+    return obj[checkProp];
+  else
+      return "Not Found";
+  
+    
 
-  result=lookup[val];
   // Only change code above this line
-  return result;
 }
-
-phoneticLookup("charlie");
