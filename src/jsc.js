cd6e30acede5938d8397850868f35f2868fb2bfa
gaurav-cedcoss <gauravshukla2022jan@cedcoss.com>
2022-02-05 15:49:09 +0530

    Golf Code

--- src/jsc.js
+++ src/jsc.js
@@ -1,19 +1,17 @@
-function testSize(num) {
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
   // Only change code below this line
-if(num<5){
-  return "Tiny";
-}
-else if(num<10)
-{return "Small";}
-else if(num<15)
-{return "Medium";}
-else if(num<20)
-{return "Large";}
-else(num>=20)
-{
-  return "Huge";
-}
+
+if(strokes==1){return "Hole-in-one!";}
+else if(strokes<=par-2){return "Eagle";}
+else if(strokes==par-1){return "Birdie";}
+else if(strokes==par){return "Par";}
+else if(strokes==par+1){return "Bogey";}
+else if(strokes==par+2){return "Double Bogey";}
+else if(strokes>=par+3){return "Go Home!";}
+  return "Change Me";
   // Only change code above this line
 }
 
-testSize(7);
+golfScore(5, 4);
