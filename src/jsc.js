abfe4a338b67812ceab227a5caeb15977e5cfeae
gaurav-cedcoss <gauravshukla2022jan@cedcoss.com>
2022-02-05 13:10:16 +0530

    Manipulate Arrays With shift()

--- src/jsc.js
+++ src/jsc.js
@@ -1,5 +1,5 @@
 // Setup
-const myArray = [["John", 23], ["cat", 2]];
+const myArray = [["John", 23], ["dog", 3]];
 
 // Only change code below this line
-const removedFromMyArray = myArray.pop(1);
+const removedFromMyArray = myArray.shift(1);
