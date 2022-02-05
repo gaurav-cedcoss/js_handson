84f9a5676f146867f9393afc9df9ee186279d24a
gaurav-cedcoss <gauravshukla2022jan@cedcoss.com>
2022-02-05 13:38:52 +0530

    Global Scope and Functions

--- src/jsc.js
+++ src/jsc.js
@@ -1,7 +1,20 @@
-function timesFive(ar){
-    return (ar*5);
+// Declare the myGlobal variable below this line
+
+let myGlobal= 10;
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
   }
-  
-  console.log(timesFive(5));
-  console.log(timesFive(2));
-  console.log(timesFive(0));
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
